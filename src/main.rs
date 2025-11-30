109a96b0d1c187e742c5dcb3fae97de0ab7b1952
Ore U <42506968+KBluePurple@users.noreply.github.com>
2025-11-30 15:26:21 +0900

    fix: tokio runtime in runtime bug

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,7 @@ struct Args {
     config: String,
 }
 
-#[tokio::main]
-async fn main() {
+fn main() {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
     let args = Args::parse();
